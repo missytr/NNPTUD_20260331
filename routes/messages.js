2a88f02b36473c7b668c9196b65f4a41d943ef3c
missytr <khanhlinhtb804@gmail.com>
2026-03-31 14:05:16 +0700

    first commit

--- routes/messages.js
+++ routes/messages.js
@@ -4,7 +4,6 @@ let messageModel = require('../schemas/message');
 let userModel = require('../schemas/users');
 let { CheckLogin } = require('../utils/authHandler');
 
-// Lấy toàn bộ tin nhắn giữa user hiện tại và userID (cả chiều gửi và nhận)
 router.get('/:userID', CheckLogin, async function (req, res, next) {
     try {
         let currentUser = req.user._id.toString();
@@ -17,14 +16,14 @@ router.get('/:userID', CheckLogin, async function (req, res, next) {
             ]
         });
 
-        // TỰ LÀM: Sắp xếp theo chiều thời gian từ cũ đến mới BẰNG CODFE JAVASCRIPT (Thay vì dùng .sort của mongo)
-        messages.sort(function(a, b) {
+
+        messages.sort(function (a, b) {
             let timeA = new Date(a.createAt || a.createdAt).getTime();
             let timeB = new Date(b.createAt || b.createdAt).getTime();
-            return timeA - timeB; 
+            return timeA - timeB;
         });
 
-        // TỰ LÀM: Tự điền thông tin người gởi người, người nhận (Thay vì dùng .populate() của mongo)
+
         let me = await userModel.findById(currentUser);
         let you = await userModel.findById(otherUser);
         let myInfo = { _id: me._id, username: me.username, email: me.email };
@@ -33,7 +32,7 @@ router.get('/:userID', CheckLogin, async function (req, res, next) {
         let resultData = messages.map(msg => {
             let fromInfo = (msg.from.toString() === currentUser) ? myInfo : yourInfo;
             let toInfo = (msg.to.toString() === currentUser) ? myInfo : yourInfo;
-            
+
             return {
                 ...msg.toObject(),
                 from: fromInfo,
@@ -53,29 +52,28 @@ router.get('/:userID', CheckLogin, async function (req, res, next) {
     }
 });
 
-// Lấy tin nhắn cuối cùng của mỗi cuộc hội thoại (Inbox list) 
+
 router.get('/', CheckLogin, async function (req, res, next) {
     try {
         let currentUser = req.user._id.toString();
 
-        // Chỉ tìm những tin rác có sự xuất hiện của mình
         let allMessages = await messageModel.find({
             $or: [
                 { from: req.user._id },
                 { to: req.user._id }
             ]
-        }); 
+        });
 
-        // TỰ LÀM: Sắp xếp MỚI NHẤT -> CŨ NHẤT bằng Javascript (Không dùng .sort() của database)
-        allMessages.sort(function(a, b) {
+
+        allMessages.sort(function (a, b) {
             let timeA = new Date(a.createAt || a.createdAt).getTime();
             let timeB = new Date(b.createAt || b.createdAt).getTime();
-            return timeB - timeA; 
+            return timeB - timeA;
         });
 
         let conversationsMap = new Map();
 
-        // TỰ LÀM: Gom nhóm bằng Javascript
+
         for (let msg of allMessages) {
             let fromId = msg.from.toString();
             let toId = msg.to.toString();
@@ -83,10 +81,9 @@ router.get('/', CheckLogin, async function (req, res, next) {
             let partnerId = (fromId === currentUser) ? toId : fromId;
 
             if (!conversationsMap.has(partnerId)) {
-                // TỰ LÀM: Đi tìm thông tin của User kia thủ công thay vì chờ Mongo .populate() chạy
                 let rawPartner = await userModel.findById(partnerId);
                 let partnerUser = null;
-                if(rawPartner) {
+                if (rawPartner) {
                     partnerUser = {
                         _id: rawPartner._id,
                         username: rawPartner.username,
@@ -116,7 +113,6 @@ router.get('/', CheckLogin, async function (req, res, next) {
     }
 });
 
-// Gửi tin nhắn mới
 router.post('/', CheckLogin, async function (req, res, next) {
     try {
         let { to, type, text } = req.body;
@@ -142,7 +138,7 @@ router.post('/', CheckLogin, async function (req, res, next) {
 
         res.status(200).send({
             success: true,
-            data: newMessage // Bỏ bớt thủ thuật dùng .populate ở đây luôn
+            data: newMessage
         });
     } catch (error) {
         res.status(500).send({
